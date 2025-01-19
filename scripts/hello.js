5787666d5bed7b03e67b0a644b5555e878310059
Titoine ⌚️🇫🇷🇨🇦 <123580047+titoinemb@users.noreply.github.com>
2025-01-19 14:12:35 -0500

    Add files via upload

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,8 +1,8 @@
 setTimeout(() => {
   document.getElementById("hellokitty").style.display = "none";
-  document.getElementById("body").style.backgroundColor = "#fff";
+  document.getElementById("body").style.backgroundColor = " #aa00ff";
   document.getElementById("body").style.display = "block";
   document.getElementById("body").style.flexDirection = "unset";
   document.getElementById("body").style.justifyContent = "unset";
-  document.getElementById("app").style.display = "block";
+  document.getElementById("app").style.display = "flex";
 }, 2000);
