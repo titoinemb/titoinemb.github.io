ac29ea0e97684e99d0e9b3695c0f4018b56143e5
Titoine ⌚️🇫🇷🇨🇦 <123580047+titoinemb@users.noreply.github.com>
2025-03-05 17:20:30 -0500

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
   document.getElementById("hellokitty").style.display = "none";
-  document.getElementById("body").style.backgroundColor = " #0096c7";
+  document.getElementById("body").style.backgroundColor = " #76ADFF";
   document.getElementById("body").style.display = "block";
   document.getElementById("body").style.flexDirection = "unset";
   document.getElementById("body").style.justifyContent = "unset";
