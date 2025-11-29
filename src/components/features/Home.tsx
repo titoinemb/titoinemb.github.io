1e9a4fda90518dcb5b9cea7c2871c7debf907e18
titoinemb <mb_titoine@proton.me>
2025-11-29 15:22:29 -0500

    build: init

---
+++ src/components/features/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>vv</div>
+  );
+}
+
+export default Home;
---
+++ src/components/features/Item.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Item() {
+  return (
+    <div>vv</div>
+  );
+}
+
+export default Item;
