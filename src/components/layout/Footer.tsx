bcb90e3f9d2c32bdca8493a4c5474a804b113808
titoinemb <mb_titoine@proton.me>
2025-12-06 13:15:28 -0500

    style: copyright text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,21 +3,24 @@ import "../../styles/layout/footer.scss";
 
 export const Footer: React.FC = () => {
   return (
-    <footer>
-      <div className="left">
-        <a href="mailto:mb_titoine@proton.me">mb_titoine@proton.me</a>
-      </div>
-      <div className="right">
-        <a href="http://github.com/titoinemb" target="_blank" rel="noopener noreferrer">
-          <img src="/svgs/github-mark-white.svg" alt="Github" width="25px" height="25px" />
-        </a>
-        <a href="https://www.npmjs.com/~titoinemb" target="_blank" rel="noopener noreferrer">
-          <img src="/svgs/npm.svg" alt="Npm" width="23px" height="23px" />
-        </a>
-        <a href="https://www.linkedin.com/in/titoine-meulenyser-bellon/" target="_blank" rel="noopener noreferrer">
-          <img src="/svgs/linkdin.svg" alt="LinkedIn" width="24px" height="24px" />
-        </a>
-      </div>
-    </footer>
+    <>
+      <footer>
+        <div className="left">
+          <a href="mailto:mb_titoine@proton.me">mb_titoine@proton.me</a>
+        </div>
+        <div className="right">
+          <a href="http://github.com/titoinemb" target="_blank" rel="noopener noreferrer">
+            <img src="/svgs/github-mark-white.svg" alt="Github" width="25px" height="25px" />
+          </a>
+          <a href="https://www.npmjs.com/~titoinemb" target="_blank" rel="noopener noreferrer">
+            <img src="/svgs/npm.svg" alt="Npm" width="23px" height="23px" />
+          </a>
+          <a href="https://www.linkedin.com/in/titoine-meulenyser-bellon/" target="_blank" rel="noopener noreferrer">
+            <img src="/svgs/linkdin.svg" alt="LinkedIn" width="24px" height="24px" />
+          </a>
+        </div>
+      </footer>
+      <div className="copyright">© 2025 Titoine MB. All rights reserved.</div>
+    </>
   );
 };
