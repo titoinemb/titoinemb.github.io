5655523085eeb40381780fcbcaabd8881b978eb6
titoinemb <mb_titoine@proton.me>
2026-01-16 13:42:28 -0500

    fix(import): remove Link import for compilation

--- src/components/features/Project.tsx
+++ src/components/features/Project.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 export const Project: React.FC = () => {
   // verifier si y a le nom du project
